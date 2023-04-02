724ba2f315f3eb89f0fe627d12595479f45cee26
Yelena <88336329+DYA13@users.noreply.github.com>
2023-04-02 15:49:21 +0200

    Week 4 assignment

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,58 +1,19 @@
 const express = require('express')
 const app = express()
-const { products } = require('./data')
 
-app.get('/', (req, res) => {
-  res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
-})
-app.get('/api/products', (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product
-    return { id, name, image }
-  })
+const people = require('./routes/people')
+const auth = require('./routes/auth')
 
-  res.json(newProducts)
-})
-app.get('/api/products/:productID', (req, res) => {
-  // console.log(req)
-  // console.log(req.params)
-  const { productID } = req.params
+// static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
 
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  )
-  if (!singleProduct) {
-    return res.status(404).send('Product Does Not Exist')
-  }
+app.use('/api/people', people)
+app.use('/login', auth)
 
-  return res.json(singleProduct)
-})
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params)
-  res.send('hello world')
-})
-
-app.get('/api/v1/query', (req, res) => {
-  // console.log(req.query)
-  const { search, limit } = req.query
-  let sortedProducts = [...products]
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search)
-    })
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit))
-  }
-  if (sortedProducts.length < 1) {
-    // res.status(200).send('no products matched your search');
-    return res.status(200).json({ sucess: true, data: [] })
-  }
-  res.status(200).json(sortedProducts)
-})
-
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000....')
+app.listen(9000, () => {
+  console.log('Server is listening on port 9000....')
 })
